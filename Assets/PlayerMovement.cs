e2e64ab5e050d67d715eabf237d9ca0d03e608cb
KarolHartwig <43039961+xGuti@users.noreply.github.com>
2022-11-07 12:33:16 +0100

    Player commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float _jumpForce;
+    private Rigidbody2D _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            _rb.velocity = new Vector2(0, _jumpForce);
+    }
+}
